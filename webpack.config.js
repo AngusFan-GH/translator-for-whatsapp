ad458c9918a3a346732e1c8ed8f1a6a0e2df0461
Angus <winterfyd@gmail.com>
2021-03-13 21:10:45 +0800

    chore: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const commonCssLoader = [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+        loader: 'postcss-loader',
+        options: {
+            ident: 'postcss',
+            plugins: () => [require('postcss-preset-env')()]
+        }
+    }
+];
+
+module.exports = {
+    entry: {
+        'background': ['./src/background/background.js'],
+        'content': ['./src/content/content.js']
+    },
+    output: {
+        path: resolve(__dirname, 'build'),
+        filename: '[name]/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [...commonCssLoader]
+            },
+            {
+                test: /\.less$/,
+                use: [...commonCssLoader, 'less-loader']
+            },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     enforce: 'pre',
+            //     loader: 'eslint-loader',
+            //     options: {
+            //         fix: true
+            //     }
+            // },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                useBuiltIns: 'usage',
+                                corejs: { version: 3 },
+                                targets: {
+                                    chrome: '60',
+                                    firefox: '50'
+                                }
+                            }
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.(jpg|png|gif)/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8 * 1024,
+                    name: '[hash:10].[ext]',
+                    outputPath: 'imgs',
+                    esModule: false
+                }
+            },
+            {
+                exclude: /\.(js|css|less|html|jpg|png|gif)/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'source'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name]/[name].css'
+        }),
+        new OptimizeCssAssetsWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './manifest.json',
+                    to: './'
+                },
+                {
+                    from: './src/icons',
+                    to: 'icons'
+                },
+            ]
+        })
+    ],
+    mode: 'production'
+}
